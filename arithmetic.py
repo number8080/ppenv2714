86294efd0297ac52d500cbd7e2997ea8af2b75ca
root <root@localhost.localdomain>
2018-02-28 13:02:18 +0800

    Initial commit

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+num1 = input("plz input your num1:")
+num2 = input("plz input your num2:")
+
+# num1 = raw_input("plz input your num1:").strip()
+# num2 = raw_input("plz input your num2:").strip()
+
+print "%s + %s = %s" %(num1,num2,num1 + num2)
+print "%s - %s = %s" %(num1,num2,num1 - num2)
+print "%s * %s = %s" %(num1,num2,num1 * num2)
+print "%s / %s = %s" %(num1,num2,num1 / num2)
+print "%s %% %s = %s" %(num1,num2,num1 % num2)
